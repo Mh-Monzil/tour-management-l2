6c6ba06912396f2daf7006532887e52a3d60e11e
Mohiuddin Monzil <monzil246@gmail.com>
2025-07-08 21:46:05 +0600

    add: env

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://dev_monzil:ma7JEpR2chybZbFz@cluster0.j6yhdqz.mongodb.net/tour-management?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(envVars.DB_URL);
     console.log("Connected to MongoDB");
 
-    server = app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is running on port ${envVars.PORT}`);
     });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
