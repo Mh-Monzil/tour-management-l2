966241c0cbc5f969d653d7133eb04ae504cd7575
Mohiuddin Monzil <monzil246@gmail.com>
2025-07-09 16:23:29 +0600

    add: implement global error handler middleware

--- src/app/modules/user/tour.controller.ts
+++ src/app/modules/user/tour.controller.ts
@@ -1,9 +1,9 @@
 import { UserServices } from "./user.service";
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const user = await UserServices.createUserService(req.body);
 
@@ -14,9 +14,7 @@ const createUser = async (req: Request, res: Response) => {
   } catch (err: any) {
     // eslint-disable-next-line no-console
     console.log(err);
-    res
-      .status(httpStatus.BAD_REQUEST)
-      .json({ message: `Something went wrong ${err.message}` });
+    next(err);
   }
 };
 
