40faab5971d1fb279d1fed91686352ea5db52433
Mh-Monzil <monzil246@gmail.com>
2025-07-20 02:11:50 +0600

    add: implement JWT authentication with token generation and role-based access control

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,10 @@
+import { envVars } from "./../../config/env";
 import bcryptjs from "bcryptjs";
 import httpStatus from "http-status-codes";
 import AppError from "../../errorHelpers/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
-import jwt from "jsonwebtoken";
+import { generateToken } from "../../utils/jwt";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
@@ -27,7 +28,11 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     role: isUserExists.role,
   };
 
-  const accessToken = jwt.sign(jwtPayload, "secret", { expiresIn: "7d" });
+  const accessToken = generateToken(
+    jwtPayload,
+    envVars.JWT_ACCESS_SECRET,
+    envVars.JWT_ACCESS_EXPIRES
+  );
 
   return {
     accessToken,
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { UserController } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { createUserZodSchema } from "./user.validation";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "./user.interface";
 
 const router = Router();
 
@@ -11,6 +13,10 @@ router.post(
   UserController.createUser
 );
 
-router.get("/all-users", UserController.getAllUsers);
+router.get(
+  "/all-users",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  UserController.getAllUsers
+);
 
 export const UserRoutes = router;
