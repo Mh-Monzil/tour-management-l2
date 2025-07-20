be29d3f23c3c69f6e008b97b3cdee8fbcc430819
Mh-Monzil <monzil246@gmail.com>
2025-07-20 11:07:13 +0600

    add: restrict access to all-users route to SUPER_ADMIN only; implement updateUser service with role-based authorization and password hashing

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,10 @@
 import httpStatus from "http-status-codes";
 import AppError from "../../errorHelpers/AppError";
-import { IAuthProvider, IUser } from "./user.interface";
+import { IAuthProvider, IUser, Role } from "./user.interface";
 import { User } from "./user.model";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 import { envVars } from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUserService = async (payload: Partial<IUser>) => {
   const { email, password, ...rest } = payload;
@@ -13,7 +14,7 @@ const createUserService = async (payload: Partial<IUser>) => {
   if (isUserExists)
     throw new AppError(httpStatus.BAD_REQUEST, "User already exists!");
 
-  const hashedPassword = await bcryptjs.hash(
+  const hashedPassword = await bcrypt.hash(
     password as string,
     Number(envVars.BCRYPT_SALT_ROUND)
   );
@@ -39,7 +40,49 @@ const getAllUsers = async () => {
   return users;
 };
 
+const updateUser = async (
+  userId: string,
+  payload: Partial<IUser>,
+  decodedToken: JwtPayload
+) => {
+  const isUserExist = await User.findById(userId);
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.NOT_FOUND, "User Not Found");
+  }
+
+  if (payload.role) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+
+    if (payload.role === Role.SUPER_ADMIN || decodedToken.role === Role.ADMIN) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+  }
+
+  if (payload.isActive || payload.isDeleted || payload.isVerified) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+  }
+
+  if (payload.password) {
+    payload.password = await bcrypt.hash(
+      payload.password,
+      Number(envVars.BCRYPT_SALT_ROUND)
+    );
+  }
+
+  const newUpdatedUser = await User.findByIdAndUpdate(userId, payload, {
+    new: true,
+  });
+
+  return newUpdatedUser;
+};
+
 export const UserServices = {
   createUserService,
   getAllUsers,
+  updateUser,
 };
