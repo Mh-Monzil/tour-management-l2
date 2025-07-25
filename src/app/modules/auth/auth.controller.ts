9c2f23025d6ec5546ecd2e6c13caa4308b14a9a0
Mh-Monzil <monzil246@gmail.com>
2025-07-26 03:00:42 +0600

    add: implement cookie-based refresh token handling; add getNewAccessToken functionality to retrieve new access tokens

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,9 +3,15 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import SendResponse from "../../utils/sendResponse";
 import { AuthServices } from "./auth.service";
+import AppError from "../../errorHelpers/AppError";
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response) => {
   const loginInfo = await AuthServices.credentialsLogin(req.body);
+
+  res.cookie("refreshToken", loginInfo.refreshToken, {
+    httpOnly: true,
+    secure: false, // Set to true if using HTTPS
+  });
 
   SendResponse(res, {
     statusCode: httpStatus.OK,
@@ -15,6 +21,22 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getNewAccessToken = catchAsync(async (req: Request, res: Response) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    throw new AppError(httpStatus.UNAUTHORIZED, "Refresh token is required");
+  }
+  const tokenInfo = await AuthServices.getNewAccessToken(refreshToken);
+
+  SendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User Logged In Successfully",
+    data: tokenInfo,
+  });
+});
+
 export const AuthControllers = {
   credentialsLogin,
+  getNewAccessToken,
 };
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import bcryptjs from "bcryptjs";
 import httpStatus from "http-status-codes";
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
+import { IsActive, IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import { createUserTokens } from "../../utils/userToken";
+import { generateToken, verifyToken } from "../../utils/jwt";
+import { envVars } from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
@@ -33,6 +36,50 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
   };
 };
 
+const getNewAccessToken = async (refreshToken: string) => {
+  const verifiedRefreshToken = verifyToken(
+    refreshToken,
+    envVars.JWT_REFRESH_SECRET
+  ) as JwtPayload;
+
+  const isUserExists = await User.findOne({
+    email: verifiedRefreshToken.email,
+  }).select("+password");
+
+  if (!isUserExists) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User does not exists!");
+  }
+  if (
+    isUserExists.isActive === IsActive.BLOCKED ||
+    isUserExists.isActive === IsActive.INACTIVE
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `User is ${isUserExists.isActive.toLowerCase()}!`
+    );
+  }
+  if (isUserExists.isDeleted) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User is deleted!");
+  }
+
+  const JwtPayload: JwtPayload = {
+    userId: isUserExists._id,
+    email: isUserExists.email,
+    role: isUserExists.role,
+  };
+
+  const accessToken = generateToken(
+    JwtPayload,
+    envVars.JWT_ACCESS_SECRET,
+    envVars.JWT_ACCESS_EXPIRES
+  );
+
+  return {
+    accessToken,
+  };
+};
+
 export const AuthServices = {
   credentialsLogin,
+  getNewAccessToken,
 };
