c9ad4c504b19adedb68d2674b8ef060f8f7c3a93
Mh-Monzil <monzil246@gmail.com>
2025-08-09 00:06:57 +0600

    add: enhance checkAuth middleware to verify user existence and status

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -1,8 +1,11 @@
+import httpStatus from "http-status-codes";
 import { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import AppError from "../errorHelpers/AppError";
 import { verifyToken } from "../utils/jwt";
 import { envVars } from "../config/env";
+import { IsActive } from "../modules/user/user.interface";
+import { User } from "../modules/user/user.model";
 
 export const checkAuth =
   (...authRoles: string[]) =>
@@ -18,6 +21,26 @@ export const checkAuth =
         accessToken,
         envVars.JWT_ACCESS_SECRET
       ) as JwtPayload;
+
+      const isUserExists = await User.findOne({
+        email: verifiedToken.email,
+      });
+
+      if (!isUserExists) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User does not exists!");
+      }
+      if (
+        isUserExists.isActive === IsActive.BLOCKED ||
+        isUserExists.isActive === IsActive.INACTIVE
+      ) {
+        throw new AppError(
+          httpStatus.BAD_REQUEST,
+          `User is ${isUserExists.isActive.toLowerCase()}!`
+        );
+      }
+      if (isUserExists.isDeleted) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User is deleted!");
+      }
 
       if (!authRoles.includes(verifiedToken.role)) {
         throw new AppError(403, "You are not permitted to view this route!");
